3635404c8b2aa38c5158b24ae0b6371656ada068
phucdz3 <95734388+phucdz3@users.noreply.github.com>
2025-08-12 11:00:51 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -36,4 +36,4 @@ getgenv().FpsBoost1 = true
     }
 getgenv().Team = "Pirates"
 getgenv().AutoLoad = false --Will Load Script On Server Hop
- loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/85e904ae1ff30824c1aa007fc7324f8f.lua"))()
